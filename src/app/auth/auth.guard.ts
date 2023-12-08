2b59b990500409e9d298f2603060d02a162e648a
Janis Blatsios <janis666@gmail.com>
2023-12-08 11:38:09 +0100

    bump

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,10 +1,4 @@
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {map, take} from 'rxjs/operators';
@@ -12,7 +6,7 @@ import {map, take} from 'rxjs/operators';
 import {AuthService} from './auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
